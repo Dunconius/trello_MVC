6a3afa875b727c73591433a58d9d84702ebfc6b4
Dunconius <duncanbean84@gmail.com>
2024-03-15 14:22:00 +1100

    authorisation added

--- controllers/card_controller.py
+++ controllers/card_controller.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from init import db
 from models.card import Card, cards_schema, card_schema
+from models.user import User
 from controllers.comment_controller import comments_bp
 
 cards_bp = Blueprint('cards', __name__, url_prefix='/cards')
@@ -52,7 +53,12 @@ def create_card():
 
 # route to delete cards - DELETE
 @cards_bp.route("/<int:card_id>", methods=["DELETE"])
+@jwt_required()
 def delete_card(card_id):
+    # check user admin status
+    is_admin = is_user_admin()
+    if not is_admin:
+        return {"error": "Not authorized to delete a card"}, 403
     # get card from database with card={card_id}
     stmt = db.select(Card).where(Card.id == card_id) # can also use filter by instead of where
     card = db.session.scalar(stmt)
@@ -71,6 +77,7 @@ def delete_card(card_id):
 # dynamic route to update cards - PUT, PATCH
 # PUT changes the whole resource. PATCH changes only the selected attributes
 @cards_bp.route("/<int:card_id>", methods=["PUT", "PATCH"])
+@jwt_required()
 def update_card(card_id):
     # get the data to be updated from the request
     body_data = card_schema.load(request.get_json(), partial=True)
@@ -79,6 +86,8 @@ def update_card(card_id):
     card = db.session.scalar(stmt)
     # if card exists:
     if card:
+        if str(card.user_id) != get_jwt_identity():
+            return {"error": "Only card owner can edit card"}, 403
         # update the fields
         # updating with the request data or leaving as is - must do all editable fields
         card.title = body_data.get('title') or card.title 
@@ -94,3 +103,8 @@ def update_card(card_id):
         # return error message
         return {'error': f"Card {card_id} not found"}, 404
     
+def is_user_admin():
+    user_id = get_jwt_identity()
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    return user.is_admin
